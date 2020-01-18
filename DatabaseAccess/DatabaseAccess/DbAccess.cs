774ecd3f636d309fe1ee7dbd0b27caca9ef449bb
GerardoAc <gera.araya3@gmail.com>
2020-01-18 16:24:31 +0100

    creacion de clases y  codigo

---
+++ DatabaseAccess/DatabaseAccess/DbAccess.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DatabaseAccess
+{
+    public abstract class DbAccess
+    {
+      public  String ConnectionString {
+            get;
+            set;
+        }
+        public MySqlTransaction DbTransaction {
+            get;
+            set;
+        } 
+       public MySqlConnection DbConnection
+        {
+            get;
+            set;
+        }
+        public abstract void OpenConnection(string ConnectionString);
+        public abstract void CloseConnection();
+        public abstract DataTable FunctionConsultSQL(String query);
+        public abstract long EjectSQL(String query);
+        public abstract Boolean IsTransaction();
+        public abstract void CommitTransaction();
+        public abstract void RollbackTransaction();
+        public abstract void BeginTransaction();
+
+    }
+}
